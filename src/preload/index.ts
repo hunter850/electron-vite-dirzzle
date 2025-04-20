c77ff542fae4e5adb63b5791493bdb1d811c4c9d
hunter850 <hunter850711@gmail.com>
2025-04-20 21:41:42 +0800

    [Style] Clean up preload index by removing commented-out code and consolidating context exports

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,31 +1,7 @@
-// import { contextBridge, ipcRenderer } from "electron";
-// import type { IpcRendererEvent } from "electron";
 import { contextBridge } from "electron";
 import windowSizeContext from "./contexts/windowSizeContext";
 import drizzleContext from "./contexts/drizzleContext";
 
-// export const electronContext = {
-//     windowSize: {
-//         listenWindowSizeStatus: (cb: (message: string) => void) => {
-//             function triggerHandler(_event: IpcRendererEvent, message: string) {
-//                 cb(message);
-//             }
-//             ipcRenderer.on("window-size-status", triggerHandler);
-//             return () => {
-//                 ipcRenderer.removeListener("window-size-status", triggerHandler);
-//             };
-//         },
-//         minimizeWindow: () => {
-//             ipcRenderer.invoke("minimize-window");
-//         },
-//         maximizeWindow: () => {
-//             ipcRenderer.invoke("maximize-window");
-//         },
-//         unmaximizeWindow: () => {
-//             ipcRenderer.invoke("unmaximize-window");
-//         },
-//     },
-// };
 export const electronContext = {
     windowSize: windowSizeContext,
     drizzle: drizzleContext,
